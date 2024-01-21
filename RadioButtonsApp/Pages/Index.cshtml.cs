ae9f04197f157d9feabbdce3fe46cdedf394fcfa
Karen Payne <paynekaren@comcast.net>
2024-01-21 07:46:35 -0800

    new code sample for forum question

---
+++ RadioButtonsApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+
+namespace RadioButtonsApp.Pages;
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+
+
+    [BindProperty]
+    public string? UserResponse { get; set; }
+
+    public string[] Answers = { "Yes", "No"};
+    public async Task<IActionResult> OnPost()
+    {
+        await Task.Delay(0);
+        var selection = UserResponse;
+        if (string.IsNullOrEmpty(selection))
+        {
+            Log.Information("No selection");
+        }
+        else
+        {
+
+            Log.Information("Choice is {C1}", selection.ToLower() == "yes");
+        }
+
+        return Page();
+    }
+}
---
+++ RadioButtonsApp/readme.md
@@ -0,0 +1,8 @@
+﻿# About
+
+Simple example of using radio buttons to ask a yes/no question without scripting. User choice shown in console window using SeriLog. If no choice, decide on what to do e.g. reprompt or accept no as the default response.
+
+:heavy_check_mark: For those who want more than strings, we can do enum and/or a model and set the default.
+
+
+![Figure1](assets/figure1.png)
